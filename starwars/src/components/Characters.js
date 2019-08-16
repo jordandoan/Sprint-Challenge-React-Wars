33015f01c36d159da2d0f60e6782e829df5d7a55
Jordan <jordandoan@hotmail.com>
2019-08-16 09:47:36 -0700

    Created Character component to display info

--- starwars/src/components/Characters.js
+++ starwars/src/components/Characters.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 import styled from "styled-components";
 import axios from "axios";
 
@@ -6,18 +6,39 @@ const Character = (props) => {
 let Card = styled.div`
     background-color:rgba(254, 183, 1, .6);
     width: 200px;
-    height:500px;
+    height:400px;
     margin: 20px;
     padding: 10px;
+    text-align:left;
 `
-let homeworld = axios.get(URL+)
+let H2 = styled.h2`
+    display:block;
+    height:50px;
+    width:100%;
+`
+const [homeworld,setHome] = useState("homeless");
+const [species,setSpecies] = useState();
+const [firstFilm, setFirst] = useState();
+useEffect(() => {
+    axios.get(props.info.homeworld)
+        .then(res => {setHome(res.data.name)});
+    axios.get(props.info.species)
+        .then(res => {setSpecies(res.data.name)});
+    axios.get(props.info.films[0])
+        .then(res => {setFirst(res.data.title)});
+}, [])
+
+
     return(
         <Card>
-            <h1>All about {props.info.name}!</h1>
+            <H2>All about {props.info.name}!</H2>
             <p>Height: {props.info.height} cm</p>
             <p>Mass: {props.info.mass} kg</p>
+            <p>Species: {species}</p>
             <p>Hair: {props.info.hair_color} </p>
-            <p>Gender: {props.info.gender}</p>
+            <p>Homeplanet: {homeworld}</p>
+            <p>Film: {firstFilm}</p>
+
         </Card>
     );
 }
